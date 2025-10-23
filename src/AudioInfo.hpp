a52dca2b267f1f402a4fa242fcc0a1b7f2662cb9
rowanmalheiro <rowan.malheiro@live.ca>
2025-10-23 18:51:07 -0400

    change pause duration

--- src/AudioInfo.hpp
+++ src/AudioInfo.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#define PAUSE_DURATION 5000
+#define PAUSE_DUR 2.5f
+#define PAUSE_DUR_I (int)(PAUSE_DUR*1000)
 
 #define NUM_SONGS 4
 
@@ -8,4 +9,4 @@ static const int OFFSET_ARR[] = {
     15500,8000,8500,8000
 };
 
-#define EFFECT_ARGS(x) 1, 0, 1, 1, true, false, OFFSET_ARR[x-1], OFFSET_ARR[x-1] + PAUSE_DURATION, 0, 0, false, 0, false, true, 0, 0, 0, 0
+#define EFFECT_ARGS(x) 1, 0, 1, 1, true, false, OFFSET_ARR[x-1], OFFSET_ARR[x-1] + PAUSE_DUR_I, 0, 0, false, 0, false, true, 0, 0, 0, 0
